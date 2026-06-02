713dd1f739dc70c428dcb90adc666acccbe5f5c2
dhruvalcodes <dhruvalp682@gmail.com>
2026-06-02 13:29:31 +0100

    spacing

--- numpy/01_basics.py
+++ numpy/01_basics.py
@@ -1,16 +1,8 @@
 import numpy as np
-
-
 arr = np.array([1, 2, 3, 4, 5])
 print("Array:", arr)
-
-
 print("Shape:", arr.shape)
-
-
 print("Type:", arr.dtype)
-
-
 matrix = np.array([[1, 2, 3], [4, 5, 6]])
 
 print("Matrix:\n", matrix)
